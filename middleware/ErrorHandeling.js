2c492fec993e6ef0a174bc49897dfbdb397ad048
abdulkemp <aakemp786@gmail.com>
2023-03-09 11:29:11 +0200

    first commit

---
+++ middleware/ErrorHandeling.js
@@ -0,0 +1,11 @@
+function errorHandeling(err, req, res, next) {
+    if(err) {
+        const status = err.status || 500;
+        res.status(status).json({
+            status: status,
+            err: 'An error occured. Please try again later.'
+        });
+    };
+    next();
+};
+module.exports = {errorHandeling};
