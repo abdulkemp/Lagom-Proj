d0e6697c0ee4c48bc5c62ff49e60ba3b63eae686
abdulkemp <aakemp786@gmail.com>
2023-03-10 16:33:12 +0200

    update

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>afascasc</h1>
-    <div class="col" v-for="item in items" :key="item.id">
-      <div class="card">
-        <img :src="item.image" class="card-img-top" alt="..."  style="width: 300px;"/>
-        <div class="card-body">
-          <h5 class="card-title">{{ item.prodName }}</h5>
-          <p class="card-text">R {{ item.price }}.00</p>
-          <button>Add to cart</button>
+    <div class="prod container">
+      <div class="row">
+        <div class="col-md-4" v-for="item in items" :key="item.id">
+          <div class="card">
+            <img :src="item.image" class="card-img-top" alt="..." style="height: 320px;width: 20rem;"/>
+            <div class="card-body">
+              <h5 class="card-title">{{ item.prodName }}</h5>
+              <p class="card-text">R {{ item.price }}</p>
+              <button>Add to cart</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -15,12 +19,12 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "products",
   data() {
     return {
-     items: this.$store.state.products,
+      items: this.$store.state.products,
     };
   },
   created() {
@@ -28,7 +32,9 @@ export default {
   },
   methods: {
     async fetchProducts() {
-      const res = await axios.get(`https://lagom-project.onrender.com/products`);
+      const res = await axios.get(
+        `https://lagom-project.onrender.com/products`
+      );
       this.items = res.data.results;
       console.log(res);
     },
@@ -36,5 +42,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+.card {
+  max-width: 30rem;
+  /* height: 10rem; */
+}
+
+.col{
+  /* justify-content: center !important;// */
+  /* align-content: center; */
+}
+.col{
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+.col-md-4{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  gap: 5px;
+}
 </style>
